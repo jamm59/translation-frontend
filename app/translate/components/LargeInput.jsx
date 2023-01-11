c1f4e20766cc9d1917f0fbd8c105adbc27f2cb6d
jam <jammoben@gmail.com>
2023-01-11 21:03:30 +0000

    readability communication

--- app/translate/components/LargeInput.jsx
+++ app/translate/components/LargeInput.jsx
@@ -4,7 +4,7 @@ export default function LargeInput({ reference }) {
     <>
       <textarea
         autoFocus
-        placeholder="Type in english the text you want to translate... "
+        placeholder='Type in english the text you want to translate and then click on "Translate" above... '
         ref={reference}
         className="w-full h-full xl:h-[80%] rounded-md drop-shadow-lg bg-gray-700 text-lg
                 resize-none outline-none text-white flex px-4 py-2 overflow-y-hidden
